aca9b32a213d9451e7e287f0b2e3b7e164331efd
R0YC0LD <77665809+R0YC0LD@users.noreply.github.com>
2025-05-13 22:08:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,41 @@
 // ASCII ile oluşturulmuş kalp şekli
 const heartPattern = [
-    "   *****       *****   ",
-    " *********   ********* ",
-    "*********** ***********",
-    "***********************",
-    " ********************* ",
-    "  *******************  ",
-    "   *****************   ",
-    "    ***************    ",
-    "     *************     ",
-    "      ***********      ",
-    "       *********       ",
-    "        *******        ",
-    "         *****         ",
-    "          ***          ",
-    "           *           "
+  "   *****       *****   ",
+  " *********   ********* ",
+  "*********** ***********",
+  "***********************",
+  " ********************* ",
+  "  *******************  ",
+  "   *****************   ",
+  "    ***************    ",
+  "     *************     ",
+  "      ***********      ",
+  "       *********       ",
+  "        *******        ",
+  "         *****         ",
+  "          ***          ",
+  "           *           "
 ];
 
 const container = document.getElementById("heart-container");
-
+const message = document.querySelector(".message");
 let currentRow = 0;
 
-// Kalp şekli ve yazıyı yavaşça ekleyen fonksiyon
 function drawHeart() {
-    if (currentRow < heartPattern.length) {
-        const row = heartPattern[currentRow];
-        const heartCells = row.split("").map(char => {
-            const div = document.createElement("div");
-            div.classList.add("heart");
-            div.innerText = char;
-            container.appendChild(div);
-            return div;
-        });
-
-        // Yavaşça kalp şekli çizilecek
-        currentRow++;
-        setTimeout(drawHeart, 300); // 300ms aralıkla sonraki satırı çiziyoruz
-    } else {
-        // Yazıyı kalbin ortasında göstermek için
-        const message = document.createElement("div");
-        message.classList.add("message");
-        message.innerText = "Onur <3 Buse";
-        container.appendChild(message);
-    }
+  if (currentRow < heartPattern.length) {
+    const row = heartPattern[currentRow];
+    row.split("").forEach(char => {
+      const div = document.createElement("div");
+      div.classList.add("heart");
+      div.innerText = char;
+      container.appendChild(div);
+    });
+    currentRow++;
+    setTimeout(drawHeart, 100);
+  } else {
+    message.innerText = "Onur <3 Buse";
+    message.style.color = "white"; // Ekstra garanti
+  }
 }
 
 drawHeart();
